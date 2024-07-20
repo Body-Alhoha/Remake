9a15c956df15b8689256f7723da105b524053ad1
n-ikko <108767629+n-ikko@users.noreply.github.com>
2024-07-20 23:29:57 +0200

    Added a useful constructor and another useful thing
    Added a constructor to construct a Transformer from a Class<?>, and  started replacing all '.' with '/' to match the ASM format in the Transformer(String) constructor

--- Core/src/main/java/fr/bodyalhoha/remake/transformer/Transformer.java
+++ Core/src/main/java/fr/bodyalhoha/remake/transformer/Transformer.java
@@ -5,8 +5,15 @@ import org.objectweb.asm.tree.ClassNode;
 public abstract class Transformer {
     private final String klass;
 
+    // Constructs a new Transformer with the specified class
+    public Transformer(Class<?> klass) {
+        this(klass.getName());
+    }
+
+    // Constructs a new Transformer with the specified class name
     public Transformer(String klass) {
-        this.klass = klass;
+        // Replace all '.' with '/' to match the ASM format
+        this.klass = klass.replace(".", "/");
     }
 
     public abstract void run(ClassNode classNode);
